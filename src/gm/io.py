c8eacb465e9d969326371e3d6f1ce82fd1cc7e3f
wjjmjh <u6269649@anu.edu.au>
2020-10-07 23:25:34 +1100

    BUILD: added IO file.

---
+++ src/gm/io.py
@@ -0,0 +1,9 @@
+# under construction
+
+
+def read_input_file(*args, **kwargs):
+    print("read_input_file func is under construction...")
+
+
+def write_output_file(*args, **kwargs):
+    print("write_output_file is under construction...")
