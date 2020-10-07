effbec7f06035ac7d56ff58a0af6e087aacd7673
wjjmjh <u6269649@anu.edu.au>
2020-10-08 02:16:45 +1100

    BUILD: initialised class vertex and edge.

--- src/gm/graph.py
+++ src/gm/graph.py
@@ -1,3 +1,13 @@
+class vertex:
+    def __init__(self):
+        print("Class Graph is under construction")
+
+
+class Edge:
+    def __init__(self):
+        print("Class Graph is under construction")
+
+
 class Graph:
     def __init__(self):
         print("Class Graph is under construction")
