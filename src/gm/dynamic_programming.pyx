948898e7cfd3f52101ac3e7e429b666a9cf15dde
wjjmjh <u6269649@anu.edu.au>
2020-10-08 01:13:31 +1100

    BUILD: added two funcs in dynamic_programming.pyx.

--- src/gm/dynamic_programming.pyx
+++ src/gm/dynamic_programming.pyx
@@ -1,1 +1,7 @@
 # Under construction
+
+def dynamic_programming_to_find_the_shortest_path():
+    print("func dynamic_programming_to_find_the_shortest_path is under construction.")
+
+def influence_of_a_vertex():
+    print("func influence_of_a_vertex is under construction.")
