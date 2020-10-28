8652fd4e90d14650bc88e966fd723da9b381970a
wjjmjh <u6269649@anu.edu.au>
2020-10-29 10:48:42 +1100

    BUILD: filled some attributes for the classes.

--- src/gm/graph.py
+++ src/gm/graph.py
@@ -2,16 +2,22 @@ class vertex:
     """
     An object having an ID and a weight to define a vertex in a graph.
     """
-    def __init__(self):
-        print("Class Graph is under construction")
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
 
 
 class Edge:
     """
     An object having two vertices as the ends and a weight to define an edge in a graph.
     """
-    def __init__(self):
-        print("Class Graph is under construction")
+    def __init__(self, vertices, cost):
+        """
+        :param vertices: a tuple that has two vertices connected by this edge. e.g. (vertex A, vertex B)
+        :param cost: the cost of going from vertex A to vertex B on this edge.
+        """
+        self.vertices = vertices
+        self.cost = cost
 
 
 class Graph:
@@ -20,12 +26,29 @@ class Graph:
     object also contains basic methods.
 
     potential methods to be implemented:
-    func get_vertices: return all the vertices of this graph;
-    func get_edges: return all the edges of this graph;
-    func set_vertices: set vertices for this graph;
-    func set_edges: set edges for this graph;
     func compute_distance_matrix: returns a distance matrix based on existing vertices and edges on the
     graph.
     """
     def __init__(self):
-        print("Class Graph is under construction")
+        self._vertices = []
+        self._edges = []
+
+    @property
+    def vertices(self):
+        return self._vertices
+
+    @vertices.setter
+    def vertices(self, new_vertices):
+        self._vertices = new_vertices
+
+    @property
+    def edges(self):
+        return self._edges
+
+    @edges.setter
+    def edges(self, new_edges):
+        self._edges = new_edges
+
+    def compute_distance_matrix(self):
+        raise NotImplementedError
+
