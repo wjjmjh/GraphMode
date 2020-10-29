8368eb4dc3d14d27dc0ef323d4afa56eeaa384f1
wjjmjh <u6269649@anu.edu.au>
2020-10-29 17:02:05 +1100

    BUILD: added numerical_pyrex.pyx.

---
+++ src/include/numerical_pyrex.pyx
@@ -0,0 +1,9 @@
+ctypedef int[::1] Int1D
+ctypedef int[:, ::1] Int2D
+ctypedef int[:, :, ::1] Int3D
+ctypedef double[::1] Double1D
+ctypedef double[:, ::1] Double2D
+ctypedef double[:, :, ::1] Double3D
+ctypedef long[::1] Long1D
+ctypedef long[:, ::1] Long2D
+ctypedef long[:, :, ::1] Long3D
