6d691e9aaa74138168f0e016418294f521c83d39
wjjmjh <u6269649@anu.edu.au>
2020-11-07 20:20:18 +1100

    MAINT: heap is not needed.

--- src/gm/heap.py
+++
@@ -1,8 +0,0 @@
-class Heap:
-    """
-    A Data Structure class that could be initialised to have data, metadata, and essential
-    operations about Heap.
-    """
-
-    def __init__(self):
-        print("class Heap is under construction.")
